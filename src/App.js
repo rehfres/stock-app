461d5f6731e86edabbaa634b9518566423774318
Grigory Moshkevich <mgm@nonamelab.ru>
2019-01-08 22:17:03 +0300

    fds

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     fetch('https://api.iextrading.com/1.0/stock/' + symbol + '/ohlc')
       .then(response => response.json())
       .then(r => {
-        // console.log('%c%s', 'color: #16a9c7', r.close.price);
+        console.log('%c%s', 'color: #c716be', r.close.price);
         this.setState(state => ({...state, previousClose: r.close.price}))
       })
       .then(() => fetch('https://api.iextrading.com/1.0/stock/' + symbol + '/chart/1d')
@@ -43,15 +43,16 @@ class App extends Component {
     console.log('%c⧭', 'color: #1663c7', pricesAll);
     priceMax = Math.max(...pricesAll, this.state.previousClose);
     priceMin = Math.min(...pricesAll, this.state.previousClose);
-    this.setState(state => ({...state, previousClose: this.state.previousClose - priceMin}))
     const coef = 35 /*canvas height */ / (priceMax - priceMin);
-    console.log('%c%s', 'color: #25c716', priceMax, priceMin, coef);
+    this.setState(state => ({...state, previousClose: (state.previousClose - priceMin) * coef}))
+    console.log('%c%s', 'color: #c716be', this.state.previousClose);
+    console.log('%c%s %s %s', 'color: #25c716', priceMax, priceMin, coef);
     const pricesModifiedAll = [];
     for (const minuteData of data) {
       if (minuteData.marketClose) pricesModifiedAll.push((minuteData.marketClose - priceMin) * coef);
     }
     this.setState(state => ({...state, prices: pricesModifiedAll}))
-    console.log('%c⧭', 'color: #c7166f', pricesModifiedAll);
+    console.log('%c⧭', 'color: #c71616', pricesModifiedAll);
     this.draw()
     
     // this.setState(state => ({ ...state, close }));
@@ -59,36 +60,42 @@ class App extends Component {
   draw() {
     const canvas = this.refs.canvas;
     const context = canvas.getContext('2d');
+    context.restore();
+    context.save();
     context.clearRect(0, 0, canvas.width, canvas.height);
     this.setState(state => ({...state, counter: state.counter + 1}))
     // if (this.state.counter > 1) return 
+    setTimeout(() => {
+
+    },2000)
     context.beginPath();
-    context.moveTo(0, this.state.previousClose);
-    context.lineTo(100, this.state.previousClose);
+    context.moveTo(0, 35 - this.state.previousClose);
+    context.lineTo(100, 35 - this.state.previousClose);
     context.lineWidth = 2;
     context.strokeStyle = '#0000001a';
     context.stroke();
 
     
     context.beginPath();
-    context.moveTo(0, this.state.previousClose);
+    context.moveTo(0, 35 - this.state.previousClose);
     for (const [index, price] of this.state.prices.entries()) {
       context.lineTo(index * 100 / 390, 35 - price);
       console.log('%c⧭', 'color: #2516c7', index * 100 / 390, price);
     }
-    context.lineTo(this.state.prices.length * 100 / 390, this.state.previousClose);
+    context.lineTo(this.state.prices.length * 100 / 390, 35 - this.state.previousClose);
     // context.closePath();
-    context.lineWidth = 2;
+    context.lineWidth = 1;
     // context.fillStyle = '#8ED6FF';
     // context.fill();
-    // context.strokeStyle = 'blue';
+    context.strokeStyle = 'rgba(0,0,0,0)';
     context.stroke();
 
+    // context.beginPath();
     context.clip();
     context.fillStyle = '#008000b2';
-    context.fillRect(0, 0, 100, this.state.previousClose);
+    context.fillRect(0, 0, 100, 35 - this.state.previousClose);
     context.fillStyle = '#ff0000b2';
-    context.fillRect(0, this.state.previousClose, 100, 35);
+    context.fillRect(0, 35 - this.state.previousClose, 100, 35);
   }
   componentDidMount() {
     // const socket = io('https://ws-api.iextrading.com/1.0/stock/chart/1d')
