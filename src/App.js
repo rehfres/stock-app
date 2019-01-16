87076868ae5a038d2e636e0555f2b25b268a99b8
Grigory Moshkevich <mgm@nonamelab.ru>
2019-01-16 14:28:29 +0300

    somewhat added firestore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Stock from './Stock';
+import * as firebase from 'firebase';
+// import 'firebase/auth';
+// import 'firebase/database';
 import './App.css';
 
 class App extends Component {
@@ -39,6 +42,46 @@ class App extends Component {
   }
   componentDidMount() {
     this.getSymbolsList().then(() => this.search(this.state.search));
+    var config = {
+      apiKey: "AIzaSyDVs10hAUStxGmb_TxSEIR7iW78NyqwVKE",
+      authDomain: "stock-app-b7556.firebaseapp.com",
+      databaseURL: "https://stock-app-b7556.firebaseio.com",
+      projectId: "stock-app-b7556",
+      storageBucket: "stock-app-b7556.appspot.com",
+      messagingSenderId: "174869200663"
+    };
+    firebase.initializeApp(config);
+    var db = firebase.firestore();
+    console.log('%c⧭', 'color: #2516c7', db);
+    db.collection("users").doc('W2vY17FP3XNxMymJS5Dz').get().then(function(doc) {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }).catch(function(error) {
+      console.log("Error getting document:", error);
+    });
+  
+    var provider = new firebase.auth.GoogleAuthProvider();
+    console.log('%c%s', 'color: #c7166f', provider);
+    // firebase.auth().signInWithPopup(provider).then(function(result) {
+    //   // This gives you a Google Access Token. You can use it to access the Google API.
+    //   var token = result.credential.accessToken;
+    //   // The signed-in user info.
+    //   var user = result.user;
+    //   // ...
+    // }).catch(function(error) {
+    //   // Handle Errors here.
+    //   var errorCode = error.code;
+    //   var errorMessage = error.message;
+    //   // The email of the user's account used.
+    //   var email = error.email;
+    //   // The firebase.auth.AuthCredential type that was used.
+    //   var credential = error.credential;
+    //   // ...
+    // });
   }
   render() {
     return (
